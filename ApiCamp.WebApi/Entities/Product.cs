cf47ba38964786e3f0e2fa5e29bde0feb4e1acc0
Eray <eray.py@gmail.com>
2025-06-18 19:41:22 +0300

    Add project files.

---
+++ ApiCamp.WebApi/Entities/Product.cs
@@ -0,0 +1,16 @@
+﻿namespace ApiCamp.WebApi.Entities
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+
+        public string ProductName { get; set; }
+
+        public string ProductDescription { get; set; }
+
+        public decimal Price { get; set; }
+
+        public string ImageUrl { get; set; }
+
+    }
+}
