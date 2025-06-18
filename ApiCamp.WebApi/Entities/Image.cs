5e2aa7b374aafa7b0a74c73df9e3487dece4d029
Eray <eray.py@gmail.com>
2025-06-18 20:06:20 +0300

    Entities

---
+++ ApiCamp.WebApi/Entities/Image.cs
@@ -0,0 +1,11 @@
+﻿namespace ApiCamp.WebApi.Entities
+{
+    public class Image
+    {
+        public int ImageId { get; set; }
+
+        public string Title { get; set; }
+
+        public string ImageUrl { get; set; }
+    }
+}
