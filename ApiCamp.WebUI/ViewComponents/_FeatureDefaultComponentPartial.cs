34bbd86d15968d5d5a8c381146bc433907070c5f
Eray <eray.py@gmail.com>
2025-06-21 11:17:33 +0300

    ViewComponents

---
+++ ApiCamp.WebUI/ViewComponents/_FeatureDefaultComponentPartial.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ApiCamp.WebUI.ViewComponents
+{
+    public class _FeatureDefaultComponentPartial : ViewComponent
+    {
+
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+
+    }
+}
